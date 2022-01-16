638c3d2c5ce10e21ba22a009e57232d258cc8a49
JooYoo <jooyoo@outlook.com>
2022-01-16 13:00:08 +0100

    Initial Commit

---
+++ swiftui-counter/swiftui_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_counterApp.swift
+//  swiftui-counter
+//
+//  Created by Yu on 2022/1/16.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
