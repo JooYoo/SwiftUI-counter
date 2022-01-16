f1dd05e30c21ab884c3e6659281416393d60b1d9
JooYoo <jooyoo@outlook.com>
2022-01-16 15:58:40 +0100

    feat: basic UI; increase, reset, decrease function;

--- swiftui-counter/ContentView.swift
+++ swiftui-counter/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count:Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(count)")
+                .padding()
+                .font(.system(size: 50, weight: .bold))
+            
+            HStack{
+                Button("-", action: {
+                    count -= 1
+                })
+                .padding()
+                .font(.system(size: 30))
+               
+                Button("reset", action: {
+                    count = 0
+                })
+                .padding()
+                .font(.system(size: 30))
+                
+                Button("+", action: {
+                    count += 1
+                })
+                .padding()
+                .font(.system(size: 30))
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDisplayName("iPhone 12")
+            .previewDevice("iPhone 12")
     }
 }
